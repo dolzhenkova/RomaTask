12c76b34f633bd3f1e3ebbc8a5a63228a6498efe
dolzhenkova <44847060+dolzhenkova@users.noreply.github.com>
2018-11-08 23:29:02 +0300

    git add

---
+++ RomaTasks/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace RomaTasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(48, 88);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1001,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(48, 33);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Нечетное натуральное число";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Число основание";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Вывод:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Проверить по 1 числу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 49);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Проверить в цикле";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 253);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RomaTasks/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomaTasks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long a, n, st;
+            a = (long)numericUpDown1.Value;
+            n = (long)numericUpDown2.Value;
+            if (n > 1) { 
+            st = (long)((Math.Pow(a, n-1))%n);
+            //Console.WriteLine(st);
+
+            // if (b > 0)
+            // {
+            if (st == 1)
+            {
+                label4.Text = "Число " + n + " псевдопростое по основанию " + (a);
+
+            }
+            else
+            {
+                label4.Text = "Число " + n + " составное";
+                label5.Text = factor1(n);
+
+            }
+
+           } else label4.Text = "Число должно быть больше 1";
+        }
+
+       
+
+        public string factor1(long m)
+        {
+            string str = m+" = 1";
+            long num = m;
+            for (int i = 0; num % 2 == 0; num /= 2)
+            {
+                str = str + "*" + 2; 
+              
+            }
+            for (int i = 3; i <= num;)
+            {
+                if (num % i == 0)
+                {
+                    str = str + "*" + i;
+                
+                    num /= i;
+                }
+                else
+                {
+                    i += 2;
+                }
+
+            }
+            //Console.WriteLine(str);
+            return str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            long a1, n, st1;
+            Boolean f = true;
+            a1 = (long)numericUpDown1.Value;
+            n = (long)numericUpDown2.Value;
+            if (a1 >= 2)
+                {
+                for (long i = a1; i >= 2; i = i - 1)
+                {
+                    st1 = (long)((Math.Pow(a1, n - 1)) % n);
+                    if (st1 != 1)
+                    {
+                        f = false;
+                        Console.WriteLine(st1);
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+                if (f) { label4.Text = "Число " + n + " псевдопростое по основанию " + (a1); }
+                else { label4.Text = "Число " + n + " составное";
+                    label5.Text = factor1(n);
+                }
+            }
+            else { label4.Text = "Основание должно быть больше 1"; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
